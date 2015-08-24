c7be60f01c367368f8f3a4af504ad9c489c5d796
MrBenGriffin <ben@redsnapper.net>
2015-08-24 20:39:24 +0100

    revision. naming class for current year report

--- do-sio.php
+++ do-sio.php
@@ -5,20 +5,28 @@ require_once("sen.inc");
 $v=new NView("view-do-sio_1.ixml");
 $err = $v->messages(); if (!empty($err)) { print_r($err); }
 $pgfn= function() use (&$v) { //profile editing.
+	$logged_in=Sec::ok();
     $v->set("//*[@data-xp='sio']/child-gap()",SIO::run());
-    if (Sec::ok(Sec::SPROFILE)) { //
-        $v->set("//*[@data-xp='title']/child-gap()","Profile");
-        $view=UserProfile::edit(Settings::$usr['ID'],NULL,true); //true=restricted
-        $view->set("//h:h2/child-gap()","– using email address: " . Settings::$usr['email']);
-        $v->set("//*[@data-xp='profile']",$view);
-    } else {
-    	if (isset(Settings::$qst[SioReg::SIG])) {
-        	$v->set("//*[@data-xp='title']/child-gap()","Register as a new user");
-    	} else {
-        	$v->set("//*[@data-xp='title']/child-gap()","Sign In/Sign Out");
-   		}
-        $v->set("//*[@data-xp='profile']");
-    }
+    Sec::user(); //set user activities.
+    SEN::user(); //set additional user data.
+	if ($logged_in==Sec::ok()) { //ie, nothing changed..
+		//  header("Location: " . Settings::$url . "?" . $this->cal . '&L&' . $this->coh );
+		if (Sec::ok(Sec::SPROFILE)) { //
+			$v->set("//*[@data-xp='title']/child-gap()","Profile");
+			$view=UserProfile::edit(Settings::$usr['ID'],NULL,true); //true=restricted
+			$view->set("//h:h2/child-gap()","– using email address: " . Settings::$usr['email']);
+			$v->set("//*[@data-xp='profile']",$view);
+		} else {
+			if (isset(Settings::$qst[SioReg::SIG])) {
+				$v->set("//*[@data-xp='title']/child-gap()","Register as a new user");
+			} else {
+				$v->set("//*[@data-xp='title']/child-gap()","Sign In/Sign Out");
+			}
+			$v->set("//*[@data-xp='profile']");
+		}
+	} else {
+		header("Location: /do-home.php");
+	}
 };
 SEN::page($pgfn,$v,0,true);
 
